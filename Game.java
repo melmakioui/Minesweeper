4730af67569470099356de649e7505102e35269d
Mohamed el Makioui <melmakioui@outlook.es>
2022-04-21 17:39:10 +0200

    Inicialitzacio de partida amb la clase Game
    Aquesta classe es la encarregada de inicialitzar la partida.

--- Game.java
+++ Game.java
@@ -1,4 +1,51 @@
 package ProjecteMinesweeper;
 
+import ProjecteMinesweeper.InputOutput.InputOutput;
+
 public class Game {
+
+    private boolean win = false;
+    private boolean lost = false;
+    private int option = 0;
+    private Grid grid;
+
+
+    public Game() {
+        this.grid = new Grid();
+
+        initGame();
+    }
+
+
+    private void initGame() {
+
+        do {
+            option = 0;
+            option = InputOutput.selectOption();
+            selectedOption();
+
+
+        } while (!win && !lost);
+
+
+    }
+
+    private void selectedOption() {
+
+        switch (option) {
+            case 1:
+                InputOutput.selectPositionXY();
+                //metode on haura de desmarcar caselles.
+                break;
+            case 2:
+                InputOutput.selectPositionXY();
+                //metode on haura de insertar una bandera
+                break;
+            case 3:
+                //metode on haure de llevar una bandera
+                break;
+        }
+    }
+
+
 }
---
+++ Main.java
@@ -0,0 +1,13 @@
+package ProjecteMinesweeper;
+
+import ProjecteMinesweeper.InputOutput.InputOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Game g = new Game();
+    }
+
+}
