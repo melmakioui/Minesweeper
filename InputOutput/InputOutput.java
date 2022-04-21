9736e1aacf80d4e65654dee9b538e827d5c56b2d
Mohamed el Makioui <melmakioui@outlook.es>
2022-04-21 17:32:28 +0200

    Implementacio classe InputOutput
    L'usuari posa la posicio on vol desmarcar
    i/o posar una bandera.

--- InputOutput/InputOutput.java
+++ InputOutput/InputOutput.java
@@ -1,4 +1,66 @@
 package ProjecteMinesweeper.InputOutput;
 
+import ProjecteMinesweeper.Game;
+
+import java.util.Scanner;
+
 public class InputOutput {
+
+    private static final Scanner input = new Scanner(System.in);
+    private static final int MIN_ROWS_COLUMNS = 0;
+    private static final int MAX_ROWS_COLUMNS = 10;
+    private static final int MIN_OPTION = 1;
+    private static final int MAX_OPTION = 3;
+
+    public static int selectOption() {
+
+
+        int option = 0;
+        chooseOptionText();
+
+        option = input.nextInt();
+
+        return validateOption(option);
+    }
+
+
+    private static int validateOption(int option) {
+
+        while (option < MIN_OPTION || option > MAX_OPTION) {
+            System.out.println("PLEASE CHOOSE AN OPTION");
+            option = input.nextInt();
+        }
+
+        return option;
+    }
+
+
+    public static int selectPositionXY() {
+        int position = 0;
+        System.out.println("POSITION");
+        position = input.nextInt();
+
+        return validatePosition(position);
+    }
+
+
+    private static int validatePosition(int position) {
+
+        while (position < MIN_ROWS_COLUMNS || position > MAX_ROWS_COLUMNS) {
+            System.out.println("PLEASE PUT A CORRECT POSITION");
+            position = input.nextInt();
+        }
+
+        return position;
+    }
+
+
+    private static void chooseOptionText() {
+        System.out.println("CHOOSE AN OPTION" +
+                "\n 1. Uncheck" +
+                "\n 2. Put Flag" +
+                "\n 3. Remove Flag");
+        System.out.print("> ");
+    }
+
 }
