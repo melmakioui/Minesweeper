1cef346ad58a7b55eb760239df70bba159c3b54c
Mohamed el Makioui <melmakioui@outlook.es>
2022-04-21 18:49:30 +0200

    Inicialitzacio de Squares i generacio de mines

--- Grid.java
+++ Grid.java
@@ -1,37 +1,65 @@
 package ProjecteMinesweeper;
 
+import java.util.Random;
+import ProjecteMinesweeper.Square;
+
 public class Grid {
 
-    private char status = '-';
     private Square squares[][];
+    private int x;
+    private int y;
 
 
-    public Grid(){
+    public Grid() {
         squares = new Square[10][10];
 
+        initSquares();
+        generateMines();
+      //  generateMines();
     }
 
 
-
-    public void displayGrid(){
+    private void initSquares(){
 
         for (int i = 0; i < squares.length; i++) {
+            for (int j = 0; j < squares[0].length ; j++) {
+                squares[i][j] = new Square();
+            }
+        }
+
+    }
+
+    private void generateMines() {
+        Random randomMines = new Random();
+
+        int counter = 0;
+
+        while (counter != squares.length + 1) {
+            x = randomMines.nextInt(squares[0].length);
+            y = randomMines.nextInt(squares.length);
+
+            squares[y][x].putMine(true);
+            counter++;
+        }
+    }
+
+
+    public void displayGrid() {
+
+        int yNumbers = 1;
+
+        for (int i = 0; i < squares.length; i++) {
+            System.out.print(yNumbers++ + "  ");
             for (int j = 0; j < squares[0].length; j++) {
-                System.out.print("[" + getStatus() + "] ");
+                System.out.print(" [" + squares[i][j].getStatus() + "] ");
             }
             System.out.println();
         }
     }
 
+    
 
-    //Getters - Setters
 
 
-    public void setStatus(char status) {
-        this.status = status;
-    }
 
-    public char getStatus() {
-        return status;
-    }
 }
