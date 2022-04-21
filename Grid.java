75eba6caf2a913ee7a742f78dc6e606bbfa0d16f
Mohamed el Makioui <melmakioui@outlook.es>
2022-04-21 18:09:02 +0200

    SemiEncapsulacio i metode displayGrid

--- Grid.java
+++ Grid.java
@@ -1,4 +1,37 @@
 package ProjecteMinesweeper;
 
 public class Grid {
+
+    private char status = '-';
+    private Square squares[][];
+
+
+    public Grid(){
+        squares = new Square[10][10];
+
+    }
+
+
+
+    public void displayGrid(){
+
+        for (int i = 0; i < squares.length; i++) {
+            for (int j = 0; j < squares[0].length; j++) {
+                System.out.print("[" + getStatus() + "] ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    //Getters - Setters
+
+
+    public void setStatus(char status) {
+        this.status = status;
+    }
+
+    public char getStatus() {
+        return status;
+    }
 }
