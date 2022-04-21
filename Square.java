f0c049296fb57a3502f7f2fdb1fc69a8568d1254
Mohamed el Makioui <melmakioui@outlook.es>
2022-04-21 18:25:30 +0200

    Encapsulacio i getters&setters Square

--- Square.java
+++ Square.java
@@ -1,4 +1,55 @@
 package ProjecteMinesweeper;
 
+import java.util.Random;
+
 public class Square {
+
+    private boolean unchecked = false;
+    private boolean marked = false; //"flag"
+    private boolean mine = false;
+    private int minesAround;
+
+
+    public Square(){
+
+
+    }
+
+
+
+
+    //Getters - Setters
+
+
+    public boolean isUnchecked() {
+        return unchecked;
+    }
+
+    public void uncheckSquare(boolean unchecked) {
+        this.unchecked = unchecked;
+    }
+
+    public boolean isMarked() {
+        return marked;
+    }
+
+    public void markSquare(boolean marked) {
+        this.marked = marked;
+    }
+
+    public boolean isMine() {
+        return mine;
+    }
+
+    public void putMine(boolean mine) {
+        this.mine = mine;
+    }
+
+    public int getMinesAround() {
+        return minesAround;
+    }
+
+    public void setMinesAround(int minesAround) {
+        this.minesAround = minesAround;
+    }
 }
