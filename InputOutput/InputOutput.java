f2a60f248f0942c4d965f5a1fbf4d5897a334a0f
Mohamed el Makioui <melmakioui@outlook.es>
2022-05-07 13:56:04 +0200

    Mod. varibale Max_Row.. per diferents nivells
    Eliminacio tambe de metodes innecessaris.

--- InputOutput/InputOutput.java
+++ InputOutput/InputOutput.java
@@ -5,30 +5,43 @@ import java.util.Scanner;
 public class InputOutput {
 
     private static final Scanner input = new Scanner(System.in);
-    private static final int MIN_ROWS_COLUMNS = 1;
-    private static final int MAX_ROWS_COLUMNS = 10;
     private static final int MIN_OPTION = 1;
     private static final int MAX_OPTION = 3;
-
     private static final int EASY = 10;
     private static final int NORMAL = 16;
     private static final int DIFFICULT = 25;
 
+    private static final int MIN_ROWS_COLUMNS = 1;
+    private static int MAX_ROWS_COLUMNS;
 
-    public static int selectLevel(){
+
+    public static int selectLevel() {
         int level;
         displayLevels();
         level = input.nextInt();
 
-        return switch (level) {
-            case 1 -> EASY;
-            case 2 -> NORMAL;
-            case 3 -> DIFFICULT;
-            default -> level;
-        };
+        switch (level) {
+            case 1 -> {
+                MAX_ROWS_COLUMNS = EASY;
+                return EASY;
+            }
+            case 2 -> {
+                MAX_ROWS_COLUMNS = NORMAL;
+                return NORMAL;
+            }
+            case 3 -> {
+                MAX_ROWS_COLUMNS = DIFFICULT;
+                return DIFFICULT;
+            }
+        }
+        MAX_ROWS_COLUMNS = EASY;
+        return EASY;
     }
 
-    public static int generateNumMines(int level){
+    ;
+
+
+    public static int generateNumMines(int level) {
 
         return switch (level) {
             case EASY -> 10;
@@ -56,7 +69,6 @@ public class InputOutput {
 
     public static int selectPositionXY() {
         int position;
-        System.out.println("COORDINATE:");
         System.out.print("> ");
         position = input.nextInt();
 
@@ -70,26 +82,11 @@ public class InputOutput {
     }
 
 
-
-    public static void displayAlreadyUncoveredCoordinate() {
-        System.out.println("¡¡¡¡THIS POSITION IS ALREADY UNCOVERED!!!!" +
-                "\n**SELECT A CORRECT CELL**");
+    public static void displayAlreadyShowedCell() {
+        System.out.println("¡¡¡¡THIS POSITION IS ALREADY SHOWED!!!!");
 
     }
 
-    public static void displayInvalidCellFlag() {
-        System.out.println();
-        System.out.println("*¡¡¡¡THIS CELL ISN'T MARKED!!!!*");
-        System.out.println();
-    }
-
-
-    public static void displayInvalidCellToMark() {
-        System.out.println();
-        System.out.println("¡¡¡THIS CELL IS ALREADY UNCOVERED, " +
-                "\n¡¡*YOU CANNOT PUT/REMOVE A FLAG IN AN UNCOVERED CELL*!!");
-        System.out.println();
-    }
 
     public static void displayWinner() {
         System.out.println("\033[0;32m" + "**YOU WIN**");
@@ -98,13 +95,12 @@ public class InputOutput {
 
     private static void displayOptions() {
         System.out.println("CHOOSE AN OPTION" +
-                "\n 1. Uncheck" +
-                "\n 2. Put Flag" +
-                "\n 3. Remove Flag");
+                "\n 1. Show" +
+                "\n 2. Put Flag/Remove Flag");
         System.out.print("> ");
     }
 
-    private static void displayLevels(){
+    private static void displayLevels() {
         System.out.println("**SELECT LEVEL**" +
                 "\n 1. Easy" +
                 "\n 2. Normal" +
